8f70cb85bad2cfdb3812201d3d6fb67a9edb9d19
evilgod <egod1430@gmail.com>
2017-04-14 18:56:13 -0700

    check if it's unique in a straing
    unique.py: using boolean array and ascii table
    unique_inspace: using sortted string and check if it's identical between
    neighbors

--- Ch1/1_1/unique.py
+++ Ch1/1_1/unique.py
@@ -1,1 +1,19 @@
-s="if this string is unique"
+s="abcde!!"
+
+def is_unique(s):
+    if len(s)>128: return False
+    
+    booleanArray=[False]*128
+    
+    for i in range(len(s)):
+        asciiVal= ord(s[i])
+        if booleanArray[asciiVal]:
+            return False
+        booleanArray[asciiVal]=True
+    
+    return True
+        
+if __name__ == "__main__":
+    print is_unique(s)  
+    
+    
---
+++ Ch1/1_1/unique_inspace.py
@@ -0,0 +1,12 @@
+s="abcde!x  "
+
+#so it first and check neighbor if it's same
+def isUnique_inspace(s):
+    
+    sortedString=''.join(sorted(s))
+    for i in range(len(s)-1):
+        if sortedString[i]==sortedString[i+1]:return False
+    return True
+
+if __name__ == "__main__":
+    print isUnique_inspace(s)
